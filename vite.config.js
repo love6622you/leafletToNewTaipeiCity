d5aab2ccb43fed948f75d05fc5f6448c972b9c65
Chia <lovemaple0913@gmail.com>
2024-01-10 02:39:00 +0800

    fix: base path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/leafletToNewTaipeiCity/',
   plugins: [vue(), eslint()],
   resolve: {
     alias: {
