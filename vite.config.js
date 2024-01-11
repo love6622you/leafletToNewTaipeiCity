708dd3d4e917094330dd3255479b21116f6e5f5c
Chia <lovemaple0913@gmail.com>
2024-01-11 19:26:13 +0800

    fix: google login

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/leafletToNewTaipeiCity/',
+  base: './',
   plugins: [vue(), eslint()],
   resolve: {
     alias: {
